f88796c57734d80bd51c9de386202c33c63e41a2
pan-maciek <kozieja.maciej@gmail.com>
2019-11-13 23:09:56 +0000

    standardized coordinates

--- src/agh/cs/lab2/Vector2d.java
+++ src/agh/cs/lab2/Vector2d.java
@@ -10,8 +10,8 @@ public class Vector2d {
 
     public boolean precedes(Vector2d other) { return x <= other.x && y <= other.y; }
     public boolean follows(Vector2d other) { return x >= other.x && y >= other.y; }
-    public Vector2d upperRight(Vector2d other) { return new Vector2d(max(x, other.x), max(y, other.y)); }
-    public Vector2d lowerLeft(Vector2d other) { return new Vector2d(min(x, other.x), min(y, other.y)); }
+    public Vector2d lowerRight(Vector2d other) { return new Vector2d(max(x, other.x), min(y, other.y)); }
+    public Vector2d upperLeft(Vector2d other) { return new Vector2d(min(x, other.x), max(y, other.y)); }
     public Vector2d add(Vector2d other)  { return new Vector2d(x + other.x, y + other.y); }
     public Vector2d subtract(Vector2d other) { return new Vector2d(x - other.x, y - other.y); }
     public Vector2d opposite() { return new Vector2d(-x, -y); }
